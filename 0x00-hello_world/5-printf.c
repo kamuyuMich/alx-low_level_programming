8120e46102c29d3861f44db47eae81f55073ce88
Mich <kamuyum01@gmail.com>
2023-07-13 00:11:50 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
 /**
  * main - This main function prints a statement using printf function
  * Return: 0 (upon success)
- * */
+ * 
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
