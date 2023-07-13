4fe9454cc570ffa583dda9a27ec9ebb9fcb646a3
Mich <kamuyum01@gmail.com>
2023-07-13 12:09:39 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,12 @@
  * main - Entry point
  * Return: 1 (fail)
  */
-int main(void) { 
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int main(void)
+{ 
+	char message[] = "and that piece of art is useful\" - Dora Korpar, \
+			  2015-10-19\n";
  	int fd = open("/dev/stderr", O_WRONLY);
 	write(fd, message, strlen(message));
 	close(fd);
-	return 1;
+	return(1);
 }
