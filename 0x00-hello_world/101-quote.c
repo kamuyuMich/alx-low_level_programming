00a0a0d86df9e70c37b84724dd6a9a79c1093cbf
Mich <kamuyum01@gmail.com>
2023-07-13 12:21:23 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
 int main(void)
 {
 	char message[] = "and that piece of art is useful\" - "
-                     "Dora Korpar, 2015-10-19\n";
+		"Dora Korpar, 2015-10-19\n";
+
 	int fd = open("/dev/stderr", O_WRONLY);
 	write(fd, message, strlen(message));
 	close(fd);
