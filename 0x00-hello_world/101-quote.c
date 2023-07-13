3795c51b1a973b2a8bd425c6cdd7af34b6c7234c
Mich <kamuyum01@gmail.com>
2023-07-13 12:05:56 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Return: 1 (fail)
+ */
+int main(void) { 
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ 	int fd = open("/dev/stderr", O_WRONLY);
+	write(fd, message, strlen(message));
+	close(fd);
+	return 1;
+}
