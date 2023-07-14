5d5b3483eaa6e602ad8a8af5284334200bdbd7ea
Mich <kamuyum01@gmail.com>
2023-07-14 08:36:24 -0700

    Upper and lower-case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+	for (i = 65; i <= 90; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
