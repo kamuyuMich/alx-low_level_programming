b55492bf06239e4a6bb03eec410a75831e4ca917
Mich <kamuyum01@gmail.com>
2023-07-14 09:57:34 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (i = 10; i < 16; i++)
+		putchar(i + 87);
+	putchar('\n');
+	return (0);
+}
