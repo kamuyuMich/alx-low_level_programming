63e0270771f9b968a2b1828263a0269092403f50
Mich <kamuyum01@gmail.com>
2023-07-18 14:51:18 -0700

    Multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,22 +7,16 @@
 
 int main(void)
 {
-	int i, sum1, sum2, result;
+	int i, result;
 
-	sum1 = 0;
-	sum2 = 0;
+	result = 0;
 	for (i = 1; i < 1024; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			sum1 = sum1 + i;
-		}
-		if (i % 5 == 0)
-		{
-			sum2 = sum2 + i;
+			result = result + i;
 		}
 	}
-	result = sum1 + sum2;
 	printf("%d\n", result);
 	return (0);
 }
