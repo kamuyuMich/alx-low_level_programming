a3f38c58a5f95b9c12bf826c85b354b70d2efebd
Mich <kamuyum01@gmail.com>
2023-07-18 09:23:19 -0700

    10 Times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times
- * Return: Always 0
  */
 
 void print_alphabet_x10(void)
@@ -14,5 +13,4 @@ void print_alphabet_x10(void)
 		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
 	_putchar('\n');
-	return (0);
 }
