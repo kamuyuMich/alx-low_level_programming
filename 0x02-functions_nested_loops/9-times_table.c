4737359ef8073c94ca446cc4356b6f3fc1746bb4
Mich <kamuyum01@gmail.com>
2023-07-18 13:39:24 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 {
 	int x, y, z, u, d;
 
-	for(x = 0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for(y = 0; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
 			if (z > 9)
@@ -24,7 +24,7 @@ void times_table(void)
 			}
 			else
 			{
-				if(y != 0)
+				if (y != 0)
 				{
 					_putchar(44);
 					_putchar(32);
