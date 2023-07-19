8f2434b98a460e4665076cbf14dbbcadb3e01b98
Mich <kamuyum01@gmail.com>
2023-07-19 14:50:44 -0700

    Positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Tests the positive or negative function for case 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+_This directory focuses on debugging_
