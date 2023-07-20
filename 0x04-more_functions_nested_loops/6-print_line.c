441606e2b89ba45c8fcf60c759813ea056fc301b
Mich <kamuyum01@gmail.com>
2023-07-20 10:01:07 -0700

    Draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a line on the terminal
+ * @n: Number of times _ should be printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(45);
+		}
+		_putchar('\n');
+	}
+}
+
