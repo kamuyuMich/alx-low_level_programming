719379df93b13040746bebc66074df3e472b68b5
Mich <kamuyum01@gmail.com>
2023-07-24 06:52:23 -0700

    Copies string using pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string
+ * @dest: Destination
+ * @src: Source
+ * Return: The pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; scr[i] != '\0'; i++)
+	{
+		dest[i] = scr[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
