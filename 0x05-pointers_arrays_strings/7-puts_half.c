a03f2123f54da9156e733e64085683fe6a9d5fc1
Mich <kamuyum01@gmail.com>
2023-07-24 05:48:42 -0700

    Put only half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: The string given
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, n, x;
+
+	x = 0;
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	x--;
+
+	if (x % 2 == 0)
+	{
+		n = x / 2;
+		for (i = n; i <= x; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		n = (x - 1) / 2;
+		for (i = n + 2; i <= x; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+}
+
