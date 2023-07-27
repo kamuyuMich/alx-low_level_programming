0fa734151986828470946d6ecd3f7cddb2c85573
Mich <kamuyum01@gmail.com>
2023-07-27 06:23:02 -0700

    Made changes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,10 @@ char *_strcat(char *dest, char *src)
 {
 	int dlen = 0, i;
 
-	while (dest[dlen]) 
+	while (dest[dlen])
 	{
 		dlen++;
 	}
-	
 	for (i = 0; src[i] != 0; i++)
 	{
 		dest[dlen] = src[i];
