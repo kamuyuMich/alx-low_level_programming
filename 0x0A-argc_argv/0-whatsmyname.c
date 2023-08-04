eebe4b6bb920fcf79a8b1ca3d2529937ef4ba0dd
Mich <kamuyum01@gmail.com>
2023-08-04 11:29:48 -0700

    Name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
