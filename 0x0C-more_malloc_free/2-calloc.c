13d1af7d09ed7eb70616b91f5098f382a7968339
Mich <kamuyum01@gmail.com>
2023-08-10 08:35:27 -0700

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@ char *_memset(char *s, char b, unsigned int n);
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -23,6 +23,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
+
+	_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
 
