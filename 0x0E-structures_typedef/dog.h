49c0fa65b7820029501753a8b40cabc514dad7fb
Mich <kamuyum01@gmail.com>
2023-08-14 10:58:37 -0700

    Dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Creates a data type dog
+ * @name: Name of the dog
+ * @age: Age of the giti
+ * @owner: Owner of the mutina
+ *
+ * Desription - This struct creates a dog type with the three variables
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+# endif
